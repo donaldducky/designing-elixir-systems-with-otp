b738c8695e562a98fdae540727640a92003d284a
Don Chea <donaldducky@gmail.com>
2020-05-15 09:11:42 -0400

    Chapter 8 Workers

--- lib/mastery/boundary/quiz_session.ex
+++ lib/mastery/boundary/quiz_session.ex
@@ -6,7 +6,7 @@ defmodule Mastery.Boundary.QuizSession do
     %{
       id: {__MODULE__, {quiz.title, email}},
       start: {__MODULE__, :start_link, [{quiz, email}]},
-      restsart: :temporary
+      restart: :temporary
     }
   end
 
@@ -28,6 +28,13 @@ defmodule Mastery.Boundary.QuizSession do
 
   def answer_question(name, answer) do
     GenServer.call(via(name), {:answer_question, answer})
+  end
+
+  def active_sessions_for(quiz_title) do
+    Mastery.Supervisor.QuizSession
+    |> DynamicSupervisor.which_children()
+    |> Enum.filter(&child_pid?/1)
+    |> Enum.flat_map(&active_sessions(&1, quiz_title))
   end
 
   def init({quiz, email}) do
@@ -54,5 +61,23 @@ defmodule Mastery.Boundary.QuizSession do
       {quiz.current_question.asked, quiz.last_response.correct},
       {quiz, email}
     }
+  end
+
+  defp child_pid?({:undefined, pid, :worker, [__MODULE__]}) when is_pid(pid) do
+    true
+  end
+
+  defp child_pid?(_child), do: false
+
+  defp active_sessions({:undefined, pid, :worker, [__MODULE__]}, title) do
+    Mastery.Registry.QuizSession
+    |> Registry.keys(pid)
+    |> Enum.filter(fn {quiz_title, _email} ->
+      quiz_title == title
+    end)
+  end
+
+  def end_sessions(names) do
+    Enum.each(names, fn name -> GenServer.stop(via(name)) end)
   end
 end
